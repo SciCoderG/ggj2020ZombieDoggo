590958ccba77b6a9b443379eb493bfe29ccba236
David Liebemann <david.liebemann@gmail.com>
2020-02-01 10:18:14 +0100

    Improved player and dog physics

--- ZombieDoggo/Assets/Scripts/Debug/TestForwardMovement.cs
+++ ZombieDoggo/Assets/Scripts/Debug/TestForwardMovement.cs
@@ -5,30 +5,34 @@ using de.crystalmesh;
 [RequireComponent(typeof(Rigidbody))]
 public class TestForwardMovement : MonoBehaviour
 {
+    //[SerializeField]
+    //private float acceleration = 20.0f;
     [SerializeField]
-    private float acceleration = 20.0f;
-    [SerializeField]
-    private float maxVelocity = 15.0f;
+    private float speed = 15.0f;
 
-    private ForceMode accelerationForceMode = ForceMode.VelocityChange;
+    //private ForceMode accelerationForceMode = ForceMode.VelocityChange;
 
-    private Rigidbody objectRigidBody = null;
+    //private Rigidbody objectRigidBody = null;
     // Start is called before the first frame update
     void Start()
     {
-        objectRigidBody = GetComponent<Rigidbody>();
+        //objectRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(objectRigidBody.velocity.magnitude < maxVelocity)
-        {
-            objectRigidBody.AddForce(Vector3.forward * acceleration * Time.fixedDeltaTime, 
-                accelerationForceMode);
-        }
-        objectRigidBody.velocity =
-                Utilities.ClampVector(
-                    objectRigidBody.velocity, Vector3.forward * maxVelocity);
+        Vector3 moveDirection = Vector3.forward;
+
+        transform.position += moveDirection * speed * Time.fixedDeltaTime;
+
+        //if(objectRigidBody.velocity.magnitude < maxVelocity)
+        //{
+        //    objectRigidBody.AddForce(Vector3.forward * acceleration * Time.fixedDeltaTime, 
+        //        accelerationForceMode);
+        //}
+        //objectRigidBody.velocity =
+        //        Utilities.ClampVector(
+        //            objectRigidBody.velocity, Vector3.forward * maxVelocity);
     }
 }
