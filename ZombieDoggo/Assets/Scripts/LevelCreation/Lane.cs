30e5714b0c7311a6aa80813090dc391b0b1770f7
David Liebemann <david.liebemann@gmail.com>
2020-02-01 19:18:01 +0100

    Added post processing

---
+++ ZombieDoggo/Assets/Scripts/LevelCreation/Lane.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    public Action OnSpawnNextLane;
+
+    [SerializeField]
+    private Transform spawnPoint = null;
+    public Transform NextLaneSpawnPoint { get { return spawnPoint; } }
+
+    public void SpawnNextLane()
+    {
+        if (null != OnSpawnNextLane)
+            OnSpawnNextLane.Invoke();
+    }
+}
--- ZombieDoggo/Assets/Scripts/LevelCreation/LaneSpawner.cs
+++ ZombieDoggo/Assets/Scripts/LevelCreation/LaneSpawner.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using de.crystalmesh;
 
 public class LaneSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] lanePrefabs = null;
+    private Lane[] lanePrefabs = null;
 
+    private List<Lane> spawnedLanes = new List<Lane>();
 
+    private void Awake()
+    {
+        spawnedLanes.AddRange(FindObjectsOfType<Lane>());
+    }
+
+    public void OnSpawnNewLane(Lane origin)
+    {
+        Lane prefabTypeToSpawn = Utilities.RandomFromArray(lanePrefabs);
+
+        Lane spawnedLane = Instantiate(prefabTypeToSpawn.gameObject).GetComponent<Lane>();
+        spawnedLane.transform.position = origin.NextLaneSpawnPoint.position;
+    }
 }
