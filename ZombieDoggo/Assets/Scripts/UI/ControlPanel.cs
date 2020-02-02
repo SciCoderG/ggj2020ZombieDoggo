2b91f5e6a1ffce39f8749c14d4327d0cd997367c
Vanessa <klappstuhlism@gmail.com>
2020-02-02 11:12:52 +0100

    ControlPanel

---
+++ ZombieDoggo/Assets/Scripts/UI/ControlPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject controlPanel = null;
+
+    [SerializeField]
+    private GameObject hudPanel = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hudPanel.SetActive(false);
+        Time.timeScale = 0;
+        StartCoroutine(startGame());
+
+    }
+
+    IEnumerator startGame()
+    {
+        yield return new WaitForSeconds(2f);
+        controlPanel.SetActive(false);
+        hudPanel.SetActive(true);
+        Time.timeScale = 1;
+
+    }
+}
