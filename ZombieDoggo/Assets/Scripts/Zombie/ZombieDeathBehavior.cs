2743ced4d5072c6640bbe0a647da69201eca60bb
David Liebemann <david.liebemann@gmail.com>
2020-02-02 12:48:22 +0100

    Camera zoom to dead zombie

--- ZombieDoggo/Assets/Scripts/Zombie/ZombieDeathBehavior.cs
+++ ZombieDoggo/Assets/Scripts/Zombie/ZombieDeathBehavior.cs
@@ -1,20 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ZombieDeathBehavior : MonoBehaviour
 {
-    
-
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private FollowDogCameraMovement followDogCamera = null;
+    [SerializeField]
+    private ZombieManagementScript zombieManagementScript = null;
+    [SerializeField]
+    private Transform OnDeathCameraPos = null;
+    [SerializeField]
+    private string HighScoreLevelName = "Highscore";
+    [SerializeField]
+    private float timeToWaitBeforeSwitchToHighscore = 5.0f;
+    private void Awake()
     {
-        
+        if (null == followDogCamera)
+            followDogCamera = FindObjectOfType<FollowDogCameraMovement>();
+    }
+    
+    public void OnDeath()
+    {
+        followDogCamera.enabled = false;
+        CameraMovement cameraMovement = followDogCamera.GetComponent<CameraMovement>();
+        cameraMovement.Target = OnDeathCameraPos.position;
+        cameraMovement.LerpToRotation(OnDeathCameraPos.rotation);
+        zombieManagementScript.PlayDeathAnimation();
+        zombieManagementScript.enabled = false;
+        zombieManagementScript.StopZombie();
+
+        StartCoroutine(SwitchToHighscoreScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SwitchToHighscoreScene()
     {
-        
+        yield return new WaitForSeconds(timeToWaitBeforeSwitchToHighscore);
+        SceneManager.LoadScene(HighScoreLevelName);
     }
 }
