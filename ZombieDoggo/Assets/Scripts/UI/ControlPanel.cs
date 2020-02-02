18786583ce2dd493a5a50d3a83244f25b4742282
Vanessa <klappstuhlism@gmail.com>
2020-02-02 11:30:30 +0100

    Freeze Game

--- ZombieDoggo/Assets/Scripts/UI/ControlPanel.cs
+++ ZombieDoggo/Assets/Scripts/UI/ControlPanel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,21 +11,23 @@ public class ControlPanel : MonoBehaviour
     [SerializeField]
     private GameObject hudPanel = null;
 
+    private float pauseEndTime;
+
     // Start is called before the first frame update
     void Start()
     {
         hudPanel.SetActive(false);
-        Time.timeScale = 0;
+        Time.timeScale = 0.01f;
+
         StartCoroutine(startGame());
-
     }
-
+    
     IEnumerator startGame()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(Time.timeScale * 2);
+
         controlPanel.SetActive(false);
         hudPanel.SetActive(true);
         Time.timeScale = 1;
-
     }
 }
