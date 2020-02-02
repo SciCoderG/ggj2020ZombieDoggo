ddb7a1ed188c048079ebd4dc536c2805bd5516a7
David Liebemann <david.liebemann@gmail.com>
2020-02-02 11:10:43 +0100

    Implemented highscore storing

---
+++ ZombieDoggo/Assets/Scripts/RandomNames.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNames
+{
+    // https://www.fantasynamegenerators.com/zombie-types.php
+    private static string[] randomZombieNames = {
+"Mumbler",
+"Chomper",
+"Friendly Zombie",
+"Tearer",
+"Mourner",
+"Nibbler",
+"Talker",
+"Learner",
+"Floater",
+"Sloucher",
+"Feeder",
+"Icicle Zombie",
+"Frenzied",
+"Popsicle",
+"Stinger",
+"Chubber",
+"Driver",
+"Smasher",
+"Suckling Zombie",
+"Grumbler",
+"Higher",
+"Wrangler",
+"Grower",
+"Scuttler",
+"Skeletal Zombie",
+"Fragment",
+"Crazed Zombie",
+"Smasher",
+"Swarmer",
+"Splasher",
+"Rotter",
+"Pack Zombie",
+"Drencher",
+"Bender",
+"Newborn",
+"Savage",
+"Fungus Zombie",
+"Shuffler",
+"Icicle Zombie",
+"Vomiter"
+    };
+
+    public static string GetRandomZombieName()
+    {
+        return de.crystalmesh.Utilities.RandomFromArray(randomZombieNames);
+    }
+}
